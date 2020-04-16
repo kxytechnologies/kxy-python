e2c184b39c115cf068576dddaf2fd3e56a6d5596
Yves-Laurent <yl@pit.ai>
2020-04-16 16:50:44 -0700

    Adding url of latest release

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ from setuptools import setup, find_packages
 setup(name='kxy',
       version='0.0',
       zip_safe=False,
+      license='AGPLv3',
+      download_url='https://github.com/kxytechnologies/kxy-python/archive/v0.0.1.tar.gz',
+      keywords = ['AutoML', 'Pre-Learning', 'Post-Learning', 'Model-Free ML'],
       packages=find_packages(exclude=['tests']),
       install_requires=['numpy>=1.13.1',
                         'pandas>=0.19.2'])
