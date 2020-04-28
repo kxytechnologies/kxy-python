2221afec0c95b9b991aeef5ef3ad9fff2409c8a2
Yves-Laurent <yl@pit.ai>
2020-04-27 21:04:01 -0700

    Updating install_requires

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ sys.path.append('.')
 from setuptools import setup, find_packages
 
 setup(name="kxy",
-	version="0.0.5",
+	version="0.0.6",
 	zip_safe=False,
 	license="AGPLv3",
     author="Dr. Yves-Laurent Kom Samo",
@@ -25,7 +25,7 @@ setup(name="kxy",
 	download_url = "https://github.com/kxytechnologies/kxy-python/archive/v0.0.3.tar.gz",
 	keywords = ["AutoML", "Pre-Learning", "Post-Learning", "Model-Free ML"],
 	packages=find_packages(exclude=["tests"]),
-	install_requires=["numpy>=1.13.1", "pandas>=0.19.2"],
+	install_requires=["numpy>=1.13.1", "pandas>=0.19.2", "pandas>=0.19.2", "requests==2.22.0"],
     classifiers=[
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Programming Language :: Python :: 3 :: Only",
