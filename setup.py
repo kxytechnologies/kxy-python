61bef9e2807467444a78cb4c6d253b54f0c90ec7
Yves-Laurent <yl@pit.ai>
2020-06-10 22:36:50 -0700

    Updating version for pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
 	long_description = f.read()
 	
-version = "0.1.0"
+version = "0.1.1"
 setup(name="kxy",
 	version=version,
 	zip_safe=False,
