a707f661b1b0daa27de5fade74ee7de1a7672d1b
Yves-Laurent <yl@pit.ai>
2020-06-11 19:40:39 -0700

    Version update for pypi release

--- kxy/__init__.py
+++ kxy/__init__.py
@@ -19,7 +19,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from kxy.api import *
 from kxy.regression import *
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
 	long_description = f.read()
 	
-version = "0.1.1"
+version = "0.1.2"
 setup(name="kxy",
 	version=version,
 	zip_safe=False,
