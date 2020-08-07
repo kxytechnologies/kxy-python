ad17051e4e6580222344a15c98d1cc2fa78b1a50
Yves-Laurent <yl@pit.ai>
2020-08-06 20:22:56 -0700

    Preparing for release

--- kxy/__init__.py
+++ kxy/__init__.py
@@ -19,7 +19,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from kxy.api import *
 from kxy.regression import *
