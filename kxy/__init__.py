0ec60481f33cdeb785441f4dfd5a15f68777bcb8
Yves-Laurent <yl@pit.ai>
2021-03-16 09:47:15 -0700

    Making the license more permissive: AGPL v3 -> GPL v3

--- kxy/__init__.py
+++ kxy/__init__.py
@@ -2,24 +2,24 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Dr. Yves-Laurent Kom Samo"
-__copyright__ = "Copyright (C) 2020 KXY Technologies, Inc."
+__copyright__ = "Copyright (C) 2021 KXY Technologies, Inc."
 __license__ = """
-Copyright (C) 2020 KXY TECHNOLOGIES, INC.
+Copyright (C) 2021 KXY TECHNOLOGIES, INC.
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+GNU General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-__version__ = "0.3.4"
+__version__ = "1.0.2"
 
 from kxy.api import *
 from kxy.pre_learning import *
--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
 	long_description = f.read()
 	
-version = "1.0.1"
+version = "1.0.2"
 setup(name="kxy",
 	version=version,
 	zip_safe=False,
-	license="AGPLv3",
+	license="GPLv3",
 	author="Dr. Yves-Laurent Kom Samo",
 	author_email="github@kxy.ai",
 	url="https://www.kxy.ai",
@@ -27,7 +27,7 @@ setup(name="kxy",
 		"Documentation": "https://www.kxy.ai/reference",
 		"Source Code": "https://github.com/kxytechnologies/kxy-python/"},
 	download_url = "https://github.com/kxytechnologies/kxy-python/archive/v%s.tar.gz" % version,
-	keywords = ["AutoML", "Pre-Learning", "Post-Learning", "Model-Free ML"],
+	keywords = ["Lean ML", "AutoML", "Pre-Learning", "Post-Learning", "Model-Free ML"],
 	packages=find_packages(exclude=["tests"]),
 	install_requires=["numpy>=1.13.1", "scipy>=1.4.1", "pandas>=0.23.0", "requests==2.22.0", \
 		"statsmodels"],
@@ -41,9 +41,9 @@ setup(name="kxy",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Information Analysis",
-		"License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
 		"Programming Language :: Python :: 3 :: Only",
-		"Development Status :: 4 - Beta",
+		"Development Status :: 5 - Production/Stable",
 		"Topic :: Scientific/Engineering :: Artificial Intelligence",
 		"Topic :: Scientific/Engineering :: Mathematics"
 	],
