04aea32df9d9f6d5158c8e5f8d8cc18d0d4f2884
Yves-Laurent <yl@kxy.ai>
2022-02-14 12:28:11 -0500

    Force describe to skip nas

--- kxy/pandas_extension/base_accessor.py
+++ kxy/pandas_extension/base_accessor.py
@@ -74,12 +74,12 @@ class BaseAccessor(object):
 						print('Other Labels: %.2f%%' % (100.-tot))
 						break
 			else:
-				m   = self._obj[col].min()
-				M   = self._obj[col].max()
-				mn  = self._obj[col].mean()
-				q50 = self._obj[col].median()
-				q25 = self._obj[col].quantile(0.25)
-				q75 = self._obj[col].quantile(0.75)
+				m   = self._obj[col].min(skipna=True, numeric_only=True)
+				M   = self._obj[col].max(skipna=True, numeric_only=True)
+				mn  = self._obj[col].mean(skipna=True, numeric_only=True)
+				q50 = self._obj[col].median(skipna=True, numeric_only=True)
+				q25 = self._obj[col].quantile(0.25, numeric_only=True)
+				q75 = self._obj[col].quantile(0.75, numeric_only=True)
 
 				print('Type:   Continuous')
 				print('Max:    %s' % ('%.1f' % M if M < 10. else '{:,}'.format(int(M))))
